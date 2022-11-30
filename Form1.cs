a761747a4de5dea393da2ce82e0902a7d00ddd99
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-30 11:36:18 +0300

    Эксперименты с pictureBox1

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace NeuralNetwork
@@ -15,6 +8,46 @@ namespace NeuralNetwork
         public Form1()
         {
             InitializeComponent();
+
+            //string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            //Graphics graphics = pictureBox1.CreateGraphics();
+
+            Bitmap bitmap = new Bitmap(500, 500);
+
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+    
+
+            Pen pen = new Pen(Color.Red, 3f);
+            graphics.DrawLine(pen, 0, 0, 0, 400);
+
+            pictureBox1.Image = bitmap;
+            //pictureBox1.Scale(new SizeF());
+
+            //pictureBox1.Load(Path.Combine(docPath, "2_3.png"));
+
+            //Graphics myCanvas = pictureBox1.CreateGraphics();
+
+            //pictureBox1.Size = new Size(50, 50);
+
+            
+
+            //myCanvas.DrawLine(new Pen(Color.Red, 3.0f), 20, 20, 30, 30);
+
+
+           
+            //int width = Convert.ToInt32(drawImage.Width);
+            //int height = Convert.ToInt32(drawImage.Height);
+
+            //int width = 50;
+            //int height = 50;
+
+            //using (Bitmap bmp = new Bitmap(width, height))
+            //{
+            //    pictureBox1.DrawToBitmap(bmp, new Rectangle(new Point(0, 0), new Size(width, height)));
+            //    bmp.Save(Path.Combine(docPath, "22563.png"), ImageFormat.Png);
+            //}
         }
     }
 }
