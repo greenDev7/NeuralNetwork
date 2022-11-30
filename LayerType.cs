839abb3d77be2f94d7602c95830e713e13d28bbb
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-30 22:12:42 +0300

    Добавлено перечисление LayerType для типов слоев (входной, скрытый и выходной)

---
+++ LayerType.cs
@@ -0,0 +1,21 @@
+﻿namespace NeuralNetwork
+{
+    /// <summary>
+    /// Тип слоя нейронной сети
+    /// </summary>
+    public enum LayerType
+    {
+        /// <summary>
+        /// Входной
+        /// </summary>
+        Input = 0,
+        /// <summary>
+        /// Скрытый
+        /// </summary>
+        Hidden = 1,
+        /// <summary>
+        /// Выходной
+        /// </summary>
+        Output = 2
+    }
+}
