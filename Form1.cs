5fe56820fda161f44f4cbc3d0234d69a6229c7b0
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-30 22:14:24 +0300

    Реализована инициализация архитектуры нейросети: задание количества нейронов на слоях, создание самих слоев, создание экземпляра класса Network и т.п.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace NeuralNetwork
@@ -7,47 +7,64 @@ namespace NeuralNetwork
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
 
-            //string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            int inputLayerDimension = 3; // Размерность (количество "нейронов") входного слоя 
+            int outputLayerDimension = 2; // Размерность выходного слоя
 
-            //Graphics graphics = pictureBox1.CreateGraphics();
+            int hiddenLayersCount = 1; // Количество скрытых слоев
+            int[] hiddenLayerDimensions = new int[hiddenLayersCount]; // Массив размерностей скрытых слоев (количество нейронов на каждом скрытом слое)
+            hiddenLayerDimensions[0] = 2;                         // Размерность первого скрытого слоя
+            // hiddenLayerDimensions[1] = 8;                      // Размерность второго скрытого слоя
+            // hiddenLayerDimensions[2] = 5;                      // Размерность третьего скрытого слоя
+            // ...
+            // и т.д.
+            // ...
+            // hiddenLayerDimensions[hiddenLayersCount - 1] = 10;  // Размерность последнего скрытого слоя
 
-            Bitmap bitmap = new Bitmap(500, 500);
+            List<double> inputSignals = new List<double>();
+            List<List<double>> outputLayerWeights = new List<List<double>>();
+            List<List<List<double>>> hiddenLayerWeights = new List<List<List<double>>>();
 
-            Graphics graphics = Graphics.FromImage(bitmap);
+            inputSignals = new List<double> { 0.0, 0.0, 1.0 };
+            outputLayerWeights = new List<List<double>>()
+            {
+                new List<double>() {-0.29, 0.73},
+                new List<double>() { 0.89, -0.53 },
+            };
 
-    
-
-            Pen pen = new Pen(Color.Red, 3f);
-            graphics.DrawLine(pen, 0, 0, 0, 400);
-
-            pictureBox1.Image = bitmap;
-            //pictureBox1.Scale(new SizeF());
-
-            //pictureBox1.Load(Path.Combine(docPath, "2_3.png"));
-
-            //Graphics myCanvas = pictureBox1.CreateGraphics();
-
-            //pictureBox1.Size = new Size(50, 50);
-
-            
-
-            //myCanvas.DrawLine(new Pen(Color.Red, 3.0f), 20, 20, 30, 30);
+            List<List<double>> firstHiddenLayerWeights = new List<List<double>>()
+            {
+                new List<double>() {0.17, -0.43, 0.68},
+                new List<double>() {0.27, 0.93, -0.13}
+            };           
 
 
-           
-            //int width = Convert.ToInt32(drawImage.Width);
-            //int height = Convert.ToInt32(drawImage.Height);
+            List<Neuron> inputNeurons = new List<Neuron>();
+            List<Neuron> outputNeurons = new List<Neuron>();
+            List<Neuron> firstHiddenLayerNeurons = new List<Neuron>();
 
-            //int width = 50;
-            //int height = 50;
+            for (int i = 0; i < inputLayerDimension; i++)
+                inputNeurons.Add(new Neuron(ActivationFunctions.Func1));
 
-            //using (Bitmap bmp = new Bitmap(width, height))
-            //{
-            //    pictureBox1.DrawToBitmap(bmp, new Rectangle(new Point(0, 0), new Size(width, height)));
-            //    bmp.Save(Path.Combine(docPath, "22563.png"), ImageFormat.Png);
-            //}
+            for (int i = 0; i < outputLayerDimension; i++)
+                outputNeurons.Add(new Neuron(ActivationFunctions.Func1, Weights: outputLayerWeights[i]));
+
+            for (int i = 0; i < hiddenLayerDimensions[0]; i++)
+                firstHiddenLayerNeurons.Add(new Neuron(ActivationFunctions.Func1, Weights: firstHiddenLayerWeights[0]));
+
+
+            Layer inputLayer = new Layer(inputNeurons, LayerType.Input);
+            Layer outputLayer = new Layer(outputNeurons, LayerType.Output);
+            Layer firstHiddenLayer = new Layer(firstHiddenLayerNeurons, LayerType.Hidden);
+
+
+            inputLayer.InputSignals = inputSignals;
+
+            Network network = new Network(inputLayer, new List<Layer> { firstHiddenLayer }, outputLayer);
         }
     }
 }
