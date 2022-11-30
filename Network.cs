477e57e5bf9b63e56e3147539bc9e6ece996e3b7
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-30 22:11:35 +0300

    Добавлен класс Network и реализован метод PropagateForward() для передачи сигнала от входного слоя до выходного

---
+++ Network.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    public class Network
+    {
+        public Layer InputLayer { get; }        
+        public List<Layer> HiddenLayers { get; }
+        public Layer OutputLayer { get; }
+
+        public Network(Layer InputLayer, List<Layer> HiddenLayers, Layer OutputLayer)
+        {
+            this.InputLayer = InputLayer;            
+            this.HiddenLayers = HiddenLayers;
+            this.OutputLayer = OutputLayer;
+        }
+
+        public List<double> PropagateForward()
+        {
+            // Получаем сигналы от входного слоя
+            List<double> signals = InputLayer.ProduceSignals();
+
+            // Передаем сигналы по скрытым слоям
+            foreach (Layer hiddenLayer in HiddenLayers)
+                signals = hiddenLayer.ProduceSignals(signals);
+
+            // Возвращаем сигналы от выходного слоя
+            return OutputLayer.ProduceSignals(signals);
+        }
+    }
+}
