c6132fe0aac4b3b04009e1be25fa0bc27392e39c
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-30 22:10:42 +0300

    Добавлен класс Layer

---
+++ Layer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get; }
+        public LayerType LayerType { get; set; }
+
+        private List<double> _inputSignals;
+        public List<double> InputSignals
+        {
+            get => _inputSignals;
+
+            set
+            {
+                if (this.LayerType != LayerType.Input)
+                    throw new Exception("Входные сигналы можно задать только для входного слоя!");
+
+                _inputSignals = value;
+            } 
+        }
+
+        public Layer(List<Neuron> Neurons, LayerType LayerType)
+        {
+            this.Neurons = Neurons;
+            this.LayerType = LayerType;
+        }
+
+        internal List<double> ProduceSignals() => InputSignals;
+
+        internal List<double> ProduceSignals(List<double> previousLayerSignals)
+        {
+            List<double> currentSignals = new List<double>();
+
+            foreach (Neuron neuron in Neurons)
+                currentSignals.Add(neuron.GetActivationPotential(previousLayerSignals));
+       
+            return currentSignals;
+        }
+    }
+}
