bbd257b8b192dca552d72165576350bb01eefed0
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-12-02 15:08:25 +0300

    Из класса Layer удалено свойство InputSignals

--- Layer.cs
+++ Layer.cs
@@ -8,27 +8,11 @@ namespace NeuralNetwork
         public List<Neuron> Neurons { get; }
         public LayerType LayerType { get; set; }
 
-        private List<double> _inputSignals;
-        public List<double> InputSignals
-        {
-            get => _inputSignals;
-
-            set
-            {
-                if (this.LayerType != LayerType.Input)
-                    throw new Exception("Входные сигналы можно задать только для входного слоя!");
-
-                _inputSignals = value;
-            } 
-        }
-
         public Layer(List<Neuron> Neurons, LayerType LayerType)
         {
             this.Neurons = Neurons;
             this.LayerType = LayerType;
         }
-
-        internal List<double> ProduceSignals() => InputSignals;
 
         internal List<double> ProduceSignals(List<double> previousLayerSignals)
         {
