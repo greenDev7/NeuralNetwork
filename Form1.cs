27d53f81735959175d69fdfbf798b9d52a527a82
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-12-02 15:09:56 +0300

    Добавлен вспомогательный класс WeightsReaderWriter для чтения/записи весовых коэффициентов из CSV-файла

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
 
 namespace NeuralNetwork
@@ -12,35 +14,22 @@ namespace NeuralNetwork
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
-            int inputLayerDimension = 3; // Размерность (количество "нейронов") входного слоя 
-            int outputLayerDimension = 2; // Размерность выходного слоя
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-            int hiddenLayersCount = 1; // Количество скрытых слоев
-            int[] hiddenLayerDimensions = new int[hiddenLayersCount]; // Массив размерностей скрытых слоев (количество нейронов на каждом скрытом слое)
-            hiddenLayerDimensions[0] = 2;                         // Размерность первого скрытого слоя
-            // hiddenLayerDimensions[1] = 8;                      // Размерность второго скрытого слоя
-            // hiddenLayerDimensions[2] = 5;                      // Размерность третьего скрытого слоя
-            // ...
-            // и т.д.
-            // ...
-            // hiddenLayerDimensions[hiddenLayersCount - 1] = 10;  // Размерность последнего скрытого слоя
+            // Инициализируем скрытые слои
+            int hiddenLayersCount = 2;
+            int[] hiddenLayersDimensions = new int[2] { 3, 2 };
+            List<List<List<double>>> hiddenLayersWeights =
+                WeightsReaderWriter.ReadHiddenLayersWeightsFromCSVFile(hiddenLayersCount, hiddenLayersDimensions, Path.Combine(docPath, "weightsOfHiddenLayers.csv"));
 
+            // Инициализируем выходной слой
+            int outputLayerDimension = 2; // Количество нейронов на выходном слое
+            List<List<double>> outputLayerWeights = WeightsReaderWriter.ReadOutputLayerWeightsFromCSVFile(Path.Combine(docPath, "weightsOfOutputLayer.csv"));
+
+
+            int inputLayerDimension = 3; // Количество "нейронов" во входном слое 
             List<double> inputSignals = new List<double>();
-            List<List<double>> outputLayerWeights = new List<List<double>>();
-            List<List<List<double>>> hiddenLayerWeights = new List<List<List<double>>>();
-
             inputSignals = new List<double> { 0.0, 0.0, 1.0 };
-            outputLayerWeights = new List<List<double>>()
-            {
-                new List<double>() {-0.29, 0.73},
-                new List<double>() { 0.89, -0.53 },
-            };
-
-            List<List<double>> firstHiddenLayerWeights = new List<List<double>>()
-            {
-                new List<double>() {0.17, -0.43, 0.68},
-                new List<double>() {0.27, 0.93, -0.13}
-            };           
 
 
             List<Neuron> inputNeurons = new List<Neuron>();
@@ -53,16 +42,13 @@ namespace NeuralNetwork
             for (int i = 0; i < outputLayerDimension; i++)
                 outputNeurons.Add(new Neuron(ActivationFunctions.Func1, Weights: outputLayerWeights[i]));
 
-            for (int i = 0; i < hiddenLayerDimensions[0]; i++)
-                firstHiddenLayerNeurons.Add(new Neuron(ActivationFunctions.Func1, Weights: firstHiddenLayerWeights[0]));
+            //for (int i = 0; i < hiddenLayersDimensions[0]; i++)
+            //    firstHiddenLayerNeurons.Add(new Neuron(ActivationFunctions.Func1, Weights: firstHiddenLayerWeights[0]));
 
 
             Layer inputLayer = new Layer(inputNeurons, LayerType.Input);
             Layer outputLayer = new Layer(outputNeurons, LayerType.Output);
             Layer firstHiddenLayer = new Layer(firstHiddenLayerNeurons, LayerType.Hidden);
-
-
-            inputLayer.InputSignals = inputSignals;
 
             Network network = new Network(inputLayer, new List<Layer> { firstHiddenLayer }, outputLayer);
         }
---
+++ WeightsReaderWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NeuralNetwork
+{
+    public static class WeightsReaderWriter
+    {
+        public static List<List<List<double>>> ReadHiddenLayersWeightsFromCSVFile(int hiddenLayersCount, int[] hiddenLayerDimensions, string weightsOfHiddenLayerFileName)
+        {
+            List<List<List<double>>> hiddenLayersWeights = new List<List<List<double>>>();
+
+            string[] lines = File.ReadAllLines(weightsOfHiddenLayerFileName);
+
+            int startPosition = 0;
+            for (int i = 0; i < hiddenLayersCount; i++)
+            {
+                List<List<double>> currentLayerWeights = new List<List<double>>();
+               
+                for (int j = startPosition; j < startPosition + hiddenLayerDimensions[i]; j++)
+                {
+                    List<double> doubleList = lines[j].Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToDouble(x)).ToList();
+                    currentLayerWeights.Add(doubleList);
+                }
+
+                startPosition += hiddenLayerDimensions[i];
+                hiddenLayersWeights.Add(currentLayerWeights);                
+            }
+
+            return hiddenLayersWeights;
+        }
+
+        public static List<List<double>> ReadOutputLayerWeightsFromCSVFile(string outputWeightsFile)
+        {
+            List<List<double>> outputLayerWeights = new List<List<double>>();
+
+            string[] lines = File.ReadAllLines(outputWeightsFile);
+
+            foreach (string line in lines)
+                outputLayerWeights.Add(line.Split(';').Select(x => Convert.ToDouble(x)).ToList());
+
+            return outputLayerWeights;
+        }
+    }
+}
