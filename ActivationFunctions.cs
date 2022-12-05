ddb9c36159a7eb398c560fe34b2f0ff6d3fe116d
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-12-05 16:34:39 +0300

    Добавлены пороговая и логистическая функции активации

--- ActivationFunctions.cs
+++ ActivationFunctions.cs
@@ -1,10 +1,17 @@
-﻿namespace NeuralNetwork
+﻿using System;
+
+namespace NeuralNetwork
 {
     public static class ActivationFunctions
     {
-        public static double Func1(double x)
+        public static double ThresholdFunction(double x)
         {
-            return 0.0;
+            return x >= 0.0 ? 1.0 : 0.0;
+        }
+
+        public static double LogisticFunction(double x, double a = 1.0)
+        {
+            return 1.0 / (1.0 + Math.Pow(Math.E, -a * x));
         }
     }
 }
