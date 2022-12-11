4c4b40f48394c08dd66e04995e460feeae6d48ca
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-12-11 14:55:49 +0300

    Убрал необязательный параметр для логистической функции в тело функции

--- ActivationFunctions.cs
+++ ActivationFunctions.cs
@@ -9,8 +9,10 @@ namespace NeuralNetwork
             return x >= 0.0 ? 1.0 : 0.0;
         }
 
-        public static double LogisticFunction(double x, double a = 1.0)
+        public static double LogisticFunction(double x)
         {
+            double a = 1.0;
+
             return 1.0 / (1.0 + Math.Pow(Math.E, -a * x));
         }
     }
