8af7972a660d0d612939e801a62b9e3ce0b84cb9
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-12-24 23:29:03 +0300

    Алгоритм обратного распространения ошибки: в класс Layer добавлены методы для расчета локальных градиентов на выходном и скрытом слое (2 перегрузки), методы для корректировки весовых коэффициетов для всех нейронов данного слоя, а также вспомогательные методы для получения связанных весовых коэффициентов и вычисления скалярного произведения (Хайкин,  стр. 231, формула 4.24)

--- Layer.cs
+++ Layer.cs
@@ -22,5 +22,50 @@ namespace NeuralNetwork
        
             return currentSignals;
         }
+
+        internal void CalculateAndSetLocalGradients(List<double> errorSignal)
+        {
+            for (int i = 0; i < Neurons.Count; i++)
+                Neurons[i].SetLocalGradient(errorSignal[i] * ActivationFunctions.LogisticFunctionsDerivative((double)Neurons[i].InducedLocalField));
+        }
+        
+        internal void CalculateAndSetLocalGradients(Layer previousLayer)
+        {
+
+            for (int i = 0; i < Neurons.Count; i++)
+            {
+                List<double> associatedWeights = GetAssociatedWeights(previousLayer, i);
+
+                double innerSum = GetInnerSum(associatedWeights, previousLayer);
+
+                Neurons[i].SetLocalGradient(innerSum * ActivationFunctions.LogisticFunctionsDerivative((double)Neurons[i].InducedLocalField));
+            }
+        }
+
+        private double GetInnerSum(List<double> associatedWeights, Layer previousLayer)
+        {
+            double innerSum = 0.0;
+
+            for (int i = 0; i < associatedWeights.Count; i++)
+                innerSum += associatedWeights[i] * previousLayer.Neurons[i].LocalGradient;
+
+            return innerSum;
+        }
+
+        private List<double> GetAssociatedWeights(Layer previousLayer, int neuronPositionInCurrentLayer)
+        {
+            List<double> associatedWeights = new List<double>();
+
+            foreach (Neuron neuron in previousLayer.Neurons)
+                associatedWeights.Add(neuron.Weights[neuronPositionInCurrentLayer]);
+
+            return associatedWeights;
+        }
+
+        internal void AdjustWeights(double learningRateParameter)
+        {
+            for (int i = 0; i < Neurons.Count; i++)
+                Neurons[i].AdjustWeights(learningRateParameter, InputSignals);
+        }
     }
 }
