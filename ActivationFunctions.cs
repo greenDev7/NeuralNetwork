fa4ccfcfa3583ea4c2b41e2332bc5106f417e07a
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-12-24 20:33:28 +0300

    Добавлена производная функции активации

--- ActivationFunctions.cs
+++ ActivationFunctions.cs
@@ -4,6 +4,8 @@ namespace NeuralNetwork
 {
     public static class ActivationFunctions
     {
+        private const double a = 1.0;        
+
         public static double ThresholdFunction(double x)
         {
             return x >= 0.0 ? 1.0 : 0.0;
@@ -11,9 +13,14 @@ namespace NeuralNetwork
 
         public static double LogisticFunction(double x)
         {
-            double a = 1.0;
-
             return 1.0 / (1.0 + Math.Pow(Math.E, -a * x));
+        }
+
+        public static double LogisticFunctionsDerivative(double x)
+        {
+            double factor = a * Math.Pow(Math.E, -a * x);
+
+            return factor * Math.Pow(LogisticFunction(x), 2.0);
         }
     }
 }
