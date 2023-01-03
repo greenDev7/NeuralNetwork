cf64448425c4c1e599fa7b5498c324ea5d7868e3
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2023-01-03 11:08:21 +0300

    Добавлены комментарии для методов класса ActivationFunctions

--- Main Classes/ActivationFunctions.cs
+++ Main Classes/ActivationFunctions.cs
@@ -4,18 +4,34 @@ namespace NeuralNetwork
 {
     public static class ActivationFunctions
     {
+        /// <summary>
+        /// Параметр для сигмоидальной функции активации
+        /// </summary>
         private const double a = 1.0;        
 
+        /// <summary>
+        /// Пороговая функция активации (использовалась для тестирования для решения проблемы XOR)
+        /// </summary>
+        /// <param name="x">аргумент функции</param>
+        /// <returns></returns>
         public static double ThresholdFunction(double x)
         {
             return x >= 0.0 ? 1.0 : 0.0;
         }
-
+        /// <summary>
+        /// Возвращает значение сигмоидальной функции активации
+        /// </summary>
+        /// <param name="x">аргумент функции</param>
+        /// <returns></returns>
         public static double SigmoidFunction(double x)
         {
             return 1.0 / (1.0 + Math.Pow(Math.E, -a * x));
         }
-
+        /// <summary>
+        /// Возвращает значение производной сигмоидальной функции активации (для алгоритма обратного распространения)
+        /// </summary>
+        /// <param name="x">аргумент функции</param>
+        /// <returns></returns>
         public static double SigmoidFunctionsDerivative(double x)
         {
             double factor = a * Math.Pow(Math.E, -a * x);
