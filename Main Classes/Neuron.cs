4ec475162f3db2df759705036fc32319af2aac01
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2023-01-03 11:29:13 +0300

    Добавлены комментарии для методов класса Neuron

--- Main Classes/Neuron.cs
+++ Main Classes/Neuron.cs
@@ -8,21 +8,31 @@ namespace NeuralNetwork
         public List<double> Weights { get; }
         public double Bias { get; }
         private Func<double, double> ActivationFunction { get; }
-        public double? InducedLocalField { get; private set; }
+        public double InducedLocalField { get; private set; }
         public double LocalGradient { get; private set; }
 
+        /// <summary>
+        /// Инициализирует нейрон
+        /// </summary>
+        /// <param name="ActivationFunction">функция активации</param>
+        /// <param name="Weights">список весовых коэффициентов</param>
+        /// <param name="Bias">пороговое значение</param>
         public Neuron(Func<double, double> ActivationFunction, List<double> Weights = null, double Bias = 0.0)
         {
             this.ActivationFunction = ActivationFunction;            
             this.Weights = Weights;
             this.Bias = Bias;
-            this.InducedLocalField = null;
+            this.InducedLocalField = 0.0;
         }        
         
+        /// <summary>
+        /// Устанавливает локальное индуцированное поля для данного нейрона
+        /// </summary>
+        /// <param name="inputSignal">входной сигнал</param>
         public void SetInducedLocalField(List<double> inputSignal)
         {
-            if (inputSignal.Count != Weights.Count)
-                throw new ArgumentOutOfRangeException("inputSignals", "Ошибка при вычислении индуцированного локального поля: количество входных сигналов не равно количеству весовых коэффициентов");
+            //if (inputSignal.Count != Weights.Count)
+            //    throw new ArgumentOutOfRangeException("inputSignals", "Ошибка при вычислении индуцированного локального поля: количество входных сигналов не равно количеству весовых коэффициентов");
 
             InducedLocalField = 0.0;
 
@@ -31,21 +41,33 @@ namespace NeuralNetwork
 
             InducedLocalField += Bias;
         }
-
+        /// <summary>
+        /// Устанавливет значение локального градиента
+        /// </summary>
+        /// <param name="localGradient">локальный градиент</param>
         internal void SetLocalGradient(double localGradient) => LocalGradient = localGradient;
-
+        /// <summary>
+        /// Корректирует весовые коэффициенты
+        /// </summary>
+        /// <param name="learningRateParameter">параметр скорости обучения</param>
+        /// <param name="inputSignals">входной сигнал нейрона, заданный при прямом проходе</param>
         internal void AdjustWeights(double learningRateParameter, List<double> inputSignals)
         {
             for (int i = 0; i < Weights.Count; i++)
                 Weights[i] += learningRateParameter * LocalGradient * inputSignals[i];
         }
-
+        /// <summary>
+        /// Возвращает функциональный сигнал от данного нейрона
+        /// </summary>
+        /// <returns></returns>
         public double GetActivationPotential()
         {
-            if (InducedLocalField == null)
-                throw new ArgumentNullException("InducedLocalField", "В функцию активации передан аргумент равный null");
-            else
-                return ActivationFunction((double)InducedLocalField);
+            //if (InducedLocalField == null)
+            //    throw new ArgumentNullException("InducedLocalField", "В функцию активации передан аргумент равный null");
+            //else
+            //    return ActivationFunction((double)InducedLocalField);
+
+            return ActivationFunction(InducedLocalField);
         }        
     }
 }
