f840926a07b0569c3d6758cf9723347d68eec776
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2023-01-03 11:02:33 +0300

    Добавлены комментарии на главную форму Form1.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,37 @@ namespace NeuralNetwork
         /// </summary>
         private void InitializeComponent()
         {
+            this.learnButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // learnButton
+            // 
+            this.learnButton.Location = new System.Drawing.Point(12, 12);
+            this.learnButton.Name = "learnButton";
+            this.learnButton.Size = new System.Drawing.Size(412, 67);
+            this.learnButton.TabIndex = 0;
+            this.learnButton.Text = "Обучить нейронную сеть";
+            this.learnButton.UseVisualStyleBackColor = true;
+            this.learnButton.Click += new System.EventHandler(this.learnButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 198);
+            this.ClientSize = new System.Drawing.Size(436, 91);
+            this.Controls.Add(this.learnButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Распознавание рукописных цифр";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button learnButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,7 @@ namespace NeuralNetwork
             InitializeComponent();            
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void learnButton_Click(object sender, EventArgs e)
         {
             string myDocumentFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
@@ -25,51 +25,65 @@ namespace NeuralNetwork
             string testImagesPath = Path.Combine(myDocumentFolder, "t10k-images-idx3-ubyte");
             string testLabelsPath = Path.Combine(myDocumentFolder, "t10k-labels-idx1-ubyte");
 
-            List<Layer> hiddenLayers = InitializeHiddenLayersWeightsFromCSVFile(Path.Combine(myDocumentFolder, "adjustedHiddenLayerWeights_acc9153.csv"));
-            Layer outputLayer = InitializeOutputLayerWeightsFromCSVFile(Path.Combine(myDocumentFolder, "adjustedOutputLayerWeights_acc9153.csv"));
+            #region Блок для инициализации нейросети с помощью весовых коэффициентов из файлов csv
 
-            Network network = new Network(hiddenLayers, outputLayer);
+            // List<Layer> hiddenLayers = InitializeHiddenLayersWeightsFromCSVFile(Path.Combine(myDocumentFolder, "adjustedHiddenLayerWeights_acc9153.csv"));
+            // Layer outputLayer = InitializeOutputLayerWeightsFromCSVFile(Path.Combine(myDocumentFolder, "adjustedOutputLayerWeights_acc9153.csv"));
 
-            #region Блок для инициализация нейросети рандомными значениями и ее обучение
-
-            ////Инициализируем нейросеть с помощью заданных параметров
-
-            //int hiddenLayersCount = 1;
-            //int[] hiddenLayersDimensions = new int[hiddenLayersCount];
-            //Func<double, double>[] hiddenActivationFunctions = new Func<double, double>[hiddenLayersCount];
-
-            //hiddenLayersDimensions[0] = 40;
-            //hiddenActivationFunctions[0] = ActivationFunctions.SigmoidFunction;
-
-            //Network network = new Network(784, 10, ActivationFunctions.SigmoidFunction, hiddenLayersDimensions, hiddenActivationFunctions);
-            //List<double> errorList = network.Train(trainingImagesPath, trainingLabelsPath, 0.2, 1);
+            // Network network = new Network(hiddenLayers, outputLayer);
 
             #endregion
 
+
+            #region Блок для инициализация нейросети рандомными значениями и ее обучение
+
+            //Инициализируем нейросеть с помощью заданных параметров
+
+            int hiddenLayersCount = 1;  // Задаем количество скрытых слоев
+            int[] hiddenLayersDimensions = new int[hiddenLayersCount]; // Массив для хранения количества нейронов на каждом скрытом слое
+            Func<double, double>[] hiddenActivationFunctions = new Func<double, double>[hiddenLayersCount]; // Массив для хранения функций активации на каждом скрытом слое
+
+            hiddenLayersDimensions[0] = 35; // У нас один скрытый слой на котором 35 нейронов
+            hiddenActivationFunctions[0] = ActivationFunctions.SigmoidFunction; // И для всех нейронов этого скрытого слоя используется сигмоидальная функция активации
+
+            // 784 входа - это размер массива полученного из изображения(28 * 28 пикселей), 10 выходных нейронов
+            Network network = new Network(784, 10, ActivationFunctions.SigmoidFunction, hiddenLayersDimensions, hiddenActivationFunctions);
+            List<double> errorList = network.Train(trainingImagesPath, trainingLabelsPath, 0.2, 1); // Запускаем обучение
+
+            #endregion
+
+
+            #region Тестируем нейросеть на тестовой выборке в 10 000 изображений
+
+            // Получаем тестовые изображения
             IEnumerable<TestCase> testCases = FileReaderMNIST.LoadImagesAndLables(testLabelsPath, testImagesPath);
 
-            int incorrectPredictionsCount = 0;
+            int incorrectPredictionsCount = 0; // счетчик неверно предсказанных результатов
             foreach (TestCase test in testCases)
             {
-                List<double> functionSignal = ImageHelper.ConvertImageToFunctionSignal(test.Image);
+                List<double> functionSignal = ImageHelper.ConvertImageToFunctionSignal(test.Image); // Преобразуем изображение в вектор размерности 784 состоящий из нулей и единичек
 
-                List<double> outputSignal = network.MakePropagateForward(functionSignal);
+                List<double> outputSignal = network.MakePropagateForward(functionSignal); // Получаем сигнал от нейросети
 
-                int predictedDigit = outputSignal.IndexOf(outputSignal.Max());
+                int predictedDigit = outputSignal.IndexOf(outputSignal.Max()); // Предсказанную цифру находим как индекс максимального элемента массива
 
+                // Если нейросеть выдала некорректный ответ
                 if (test.Label != predictedDigit)
                 {
                     incorrectPredictionsCount++;
-                    Bitmap bitmap = ImageHelper.CreateBitmapFromMnistImage(test.Image);
-                    bitmap.Save(Path.Combine(myDocumentFolder, "IncorrectPredictions", $"{incorrectPredictionsCount}_{test.Label}_{predictedDigit}.png"));
+                    //Bitmap bitmap = ImageHelper.CreateBitmapFromMnistImage(test.Image); // Получим это изображение
+                    // И сохраним в папку IncorrectPredictions
+                    //bitmap.Save(Path.Combine(myDocumentFolder, "IncorrectPredictions", $"{incorrectPredictionsCount}_{test.Label}_{predictedDigit}.png"));
                 }
             }
 
-            double accuracy = 100.0 - (incorrectPredictionsCount / 100.0);
+            double accuracy = 100.0 - (incorrectPredictionsCount / 100.0); // Вычисляем точность (%)
+            #endregion
 
-            // Записываем оптимизированные весовые коэффициенты в файлы
-            //network.WriteHiddenWeightsToCSVFile(Path.Combine(myDocumentFolder, "adjustedHiddenLayerWeights_accXX.csv"));
-            //network.WriteOutputWeightsToCSVFile(Path.Combine(myDocumentFolder, "adjustedOutputLayerWeights_accXX.csv"));
+
+            // Записываем скорректированные весовые коэффициенты в файлы
+            network.WriteHiddenWeightsToCSVFile(Path.Combine(myDocumentFolder, "adjustedHiddenLayerWeights_accXX.csv"));
+            network.WriteOutputWeightsToCSVFile(Path.Combine(myDocumentFolder, "adjustedOutputLayerWeights_accXX.csv"));
         }
 
         /// <summary>
